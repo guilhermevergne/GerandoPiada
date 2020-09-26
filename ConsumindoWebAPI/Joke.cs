be3ba295b6041aa5d2066bd5d4784e1152021713
guilhermevergne <guilhermevergne@gmail.com>
2020-09-26 20:27:04 -0300

    Finish

---
+++ ConsumindoWebAPI/Joke.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumindoWebAPI
+{
+    public class Flags
+    {
+        public bool nsfw { get; set; }
+        public bool religious { get; set; }
+        public bool political { get; set; }
+        public bool racist { get; set; }
+        public bool sexist { get; set; }
+
+        public string getflags()
+        {
+            string flags = "";
+            if (nsfw)
+            {
+                flags += "nsfw/";
+            }
+            if (religious)
+            {
+                flags += "religious/";
+            }
+            if (political)
+            {
+                flags += "political/";
+            }
+            if (racist)
+            {
+                flags += "racist/";
+            }
+            if (sexist)
+            {
+                flags += "sexist/";
+            }
+            return flags;
+        }
+    }
+    public class Joke
+    {
+        public bool error { get; set; }
+        public string category { get; set; }
+        public string type { get; set; }
+        public string joke { get; set; }
+        public string setup { get; set; }
+        public string delivery { get; set; }
+        public Flags flags { get; set; }
+        public int id { get; set; }
+        public string lang { get; set; }
+
+        public void printJoke()
+        {
+            if(flags.getflags() == "")
+            {
+                Console.WriteLine("No Flags");
+            }
+            else
+            {
+
+                Console.WriteLine($"Flags: {flags.getflags()}");
+            }
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Category: {category}");
+            if (type == "single")
+            {
+                Console.WriteLine($"Joke: {joke}");
+            }
+            if (type == "twopart")
+            {
+                Console.WriteLine($"Setup: {setup}");
+                Console.WriteLine($"Delivery: {delivery}");
+            }
+        }
+    }
+}
---
+++ ConsumindoWebAPI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+/*  Enunciado proposto pelo professor Marcos Lapa
+Vamos agora praticar as habilidades de consumo de Web APIs, consumindo uma Web API de piadas (free).
+Em anexo temos o link para a documentação da API.
+Crie uma aplicação (com interface à sua escolha: Console, Web, Mobile...) para permitir a exibição de uma piada sorteada por vez e por categoria: Programming, Miscellaneous, Dark ou Pun
+https://sv443.net/jokeapi/v2
+ */
+
+namespace ConsumindoWebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a categoria da piada ou deixe-a em branco acaso queira buscar qualquer piada!");
+            Console.WriteLine("Opções: Programming / Miscellaneous / Dark / Pun");
+            string category = Console.ReadLine();
+            if(category == "Programming" || category == "Miscellaneous" || category == "Dark" || category == "Pun" || category == "")
+            {
+                if(category == "")
+                {
+                    category = "Any";
+                }
+                GetJoke(category);
+            }
+            else
+            {
+                Console.WriteLine("Ta de brincation?");
+            }
+            Console.ReadKey();
+        }
+
+        public static async void GetJoke(string category)
+        {
+            HttpClient httpClient = new HttpClient();
+            string url = $"https://sv443.net/jokeapi/v2/joke/{category}";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            string dados = await response.Content.ReadAsStringAsync();
+
+
+            Joke p = JsonConvert.DeserializeObject<Joke>(dados);
+
+            p.printJoke();
+
+        }
+
+    }
+}
